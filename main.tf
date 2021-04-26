86683561ebcd6f4be3c14cbe4db2da04e076ab8a
JackPawsey <jfrewer@live.co.uk>
2021-04-26 14:19:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 module "calm" {
-  source = "git@gitlab.com:simply-calm/terraform-module.git?ref=v1.0"
+  source = "git@gitlab.com:simply-calm/terraform-module.git"
   
   name = "calm"
 
@@ -22,7 +22,7 @@ module "calm" {
   vpc_cidr = "10.0.0.0/16"
 
   subnets = { # "prod" elasticsearch_configuration requires 3 subnets in different AZ's
-    eu-west-1a = "10.0.1.0/24"
+    eu-west-1a = "20.0.1.0/24"
   }
 
   zone_id = "Z06102122M7792QQSFCAD"
@@ -35,9 +35,16 @@ module "calm" {
   
   grafana_instance_type = "t2.small"
 
-  elasticsearch_configuration = "staging" # "staging" - 1 t2.small.elasticsearch or "prod" - 3 m5.large.elasticsearch
+  # Elasticsearch Config ####################
 
-  # es_days_to_retain = 5
+  # TESTING ##
+  es_data_instance_type = "t2.small.elasticsearch"
+  es_data_node_count = 1
+  
+  ebs_volume_size = 10
+
+  es_rest_encryption = false # defaults true (can't be used with t2.small)
+  es_node2node_encryption = false # defaults true (can't be used with t2.small)
 }
 
 output "env" {
@@ -45,9 +52,24 @@ output "env" {
   description = "The deployment environment"
 }
 
+output "region" {
+  value       = module.calm.region
+  description = "The deployment region"
+}
+
+output "name" {
+  value       = module.calm.name
+  description = "The name of deployment"
+}
+
 output "logstash_instance_id" {
   value       = module.calm.logstash_instance_id
   description = "Logstash instance ID"
+}
+
+output "logstash_instance_ip" {
+  value       = module.calm.logstash_instance_ip
+  description = "Logstash instance IP"
 }
 
 output "prometheus_instance_id" {
@@ -55,14 +77,29 @@ output "prometheus_instance_id" {
   description = "Prometheus instance ID"
 }
 
+output "prometheus_instance_ip" {
+  value       = module.calm.prometheus_instance_ip
+  description = "Prometheus instance IP"
+}
+
 output "alertmanager_instance_id" {
   value       = module.calm.alertmanager_instance_id
   description = "Alertmanager instance ID"
 }
 
+output "alertmanager_instance_ip" {
+  value       = module.calm.alertmanager_instance_ip
+  description = "Alertmanager instance IP"
+}
+
 output "grafana_instance_id" {
   value       = module.calm.grafana_instance_id
   description = "Grafana instance ID"
+}
+
+output "grafana_instance_ip" {
+  value       = module.calm.grafana_instance_ip
+  description = "Grafana instance IP"
 }
 
 output "s3_bucket_name" {
